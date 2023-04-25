3a7cf0928015373551877ef54ea27dfc8e0e4896
amiola <agumiola@gmail.com>
2023-04-25 15:51:06 -0300

    LS1

--- context/Provider.jsx
+++ context/Provider.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Context from './Context'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Provider = ({children}) => {
 
@@ -20,6 +21,40 @@ const Provider = ({children}) => {
         fav.canonical_isbn!==id
       )))
   }
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('01', jsonValue)
+    } catch (e) {
+      console.log(`Error: ${e}`)
+    }
+  }
+
+  
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('01')
+    return jsonValue != null ? JSON.parse(jsonValue) : [];
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+useEffect(()=>{
+storeData(favs)
+},[favs])
+
+const takeData = async()=>{
+  data = await getData();
+  console.log(data)
+  return data;
+}
+takeData()
+
+// (()=>{
+//   setFavs(takeData())
+// })()
 
   return (
     <Context.Provider
